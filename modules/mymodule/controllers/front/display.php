9be558da8180e9c0e064fceb0233812245ace948
Bimai6 <bimaivfx@gmail.com>
2025-05-27 09:13:07 +0200

    feat: add translation enable for my module message

--- modules/mymodule/controllers/front/display.php
+++ modules/mymodule/controllers/front/display.php
@@ -8,7 +8,7 @@ class mymoduledisplayModuleFrontController extends ModuleFrontController
             [
                 'my_module_name' => Configuration::get('MYMODULE_NAME'),
                 'my_module_link' => $this->context->link->getModuleLink('mymodule', 'display'),
-                'my_module_message' => $this->l('This is a simple text message') // Do not forget to enclose your strings in the l() translation method
+                'my_module_message' => $this->trans('This is a simple text message', [], 'Modules.MyModule.Admin') // Do not forget to enclose your strings in the l() translation method
             ]
         );
         $this->setTemplate('module:mymodule/views/templates/front/display.tpl');
--- modules/mymodule/views/templates/front/display.tpl
+++ modules/mymodule/views/templates/front/display.tpl
@@ -1,4 +1,4 @@
 {extends file='page.tpl'}
 {block name='page_content'}
-    {$my_module_message}
+    {$my_module_message|trans({}, 'Modules.MyModule.Shop')}
 {/block}
